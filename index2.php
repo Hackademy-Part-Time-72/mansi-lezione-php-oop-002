326a03c1fafa9c177b9ee1eb185c77f738ca5189
francesco mansi <f.mansi@socialwebsolutions.it>
2026-01-09 20:47:11 +0100

    Aggiunta prima parte 2 di oop2

---
+++ index2.php
@@ -0,0 +1,68 @@
+<?php
+class Person
+{
+    private string $firstname;
+    public string $lastname;
+    // protected string $password; //2
+
+    public function __construct(string $string1, string $string2)
+    {
+
+        $this->firstname = $string1;
+        $this->lastname = $string2;
+    }
+}
+
+class Teacher extends Person
+{
+    public int $work_hour;
+    public function __construct(string $string1, string $string2, int $integer,)
+    {
+        parent::__construct($string1, $string2);
+        $this->work_hour = $integer;
+    }
+
+    public function hello()
+    {
+        echo "Ciao mi chiamo {$this->firstname} {$this->lastname}, sono un docente e lavoro {$this->work_hour} ore.\n";
+    }
+
+
+    public function getFirstname()
+    {
+        echo $this->firstname;
+    }
+
+    public function setFirstname($string)
+    {
+        // $is_admin = true;
+        // if ($is_admin) {
+        $this->firstname = $string;
+        // } else {
+        //     echo "Non sei un admin";
+        // }
+    }
+}
+
+class Student extends Person
+{
+    public float $avg;
+
+    public function __construct(string $string1, string $string2, float $floating)
+    {
+        parent::__construct($string1, $string2);
+        $this->avg = $floating;
+    }
+
+    public function hello()
+    {
+        echo "Ciao mi chiamo {$this->firstname} {$this->lastname}, sono uno studente e ho la media voti di {$this->avg}.\n";
+    }
+}
+
+
+$person = new Teacher('Francesco', 'Mansi', 89);
+$person->setFirstname('Jack');
+$person->getFirstname();
+// $person->password = 'Ciccio';
+// echo $person->password;
